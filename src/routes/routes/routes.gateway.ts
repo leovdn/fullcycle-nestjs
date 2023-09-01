f98b575e41790abea302985732d4ed7e8ccd97df
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2023-09-01 13:44:30 +0000

    chore: install redis

--- src/routes/routes/routes.gateway.ts
+++ src/routes/routes/routes.gateway.ts
@@ -1,5 +1,6 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
+import { RoutesDriverService } from '../routes-driver/routes-driver.service';
 
 @WebSocketGateway({
   cors: {
@@ -7,11 +8,14 @@ import { Socket } from 'socket.io';
   },
 })
 export class RoutesGateway {
+  constructor(private routesDriverService: RoutesDriverService) {}
+
   @SubscribeMessage('new-points')
-  handleMessage(
+  async handleMessage(
     client: Socket,
     payload: { route_id: string; lat: number; lng: number },
   ) {
+    await this.routesDriverService.createOrUpdate(payload);
     client.broadcast.emit('admin-new-points', payload);
     client.broadcast.emit(`new-points/${payload.route_id}`, payload);
   }
